5a6f89ecf50f2e4ece167f367632fd96b5748f9c
NHN <nhn@NHNui-MacBookPro.local>
2021-01-25 00:49:34 +0900

    feature_4 commit

--- mreview/src/main/java/org/zerock/mreview/controller/UploadController.java
+++ mreview/src/main/java/org/zerock/mreview/controller/UploadController.java
@@ -93,7 +93,7 @@ public class UploadController {
     }
 
     @GetMapping("/display")
-    public ResponseEntity<byte[]> getFile(String fileName){
+    public ResponseEntity<byte[]> getFile(String fileName, String size){
 
         ResponseEntity<byte[]> result = null;
 
@@ -104,6 +104,9 @@ public class UploadController {
 
             File file = new File(uploadPath + File.separator+srcFileName);
 
+            if(size != null && size.equals("1")){
+                file = new File(file.getParent(), file.getName().substring(2));
+            }
             log.info("file: "+file);
 
             HttpHeaders header = new HttpHeaders();
